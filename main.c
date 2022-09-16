78b3e011d34547428edc874225f8e802b99e37a7
Ryan van den Bogaard <bogaardryan@hotmail.com>
2022-09-16 14:43:39 +0200

    blank game

---
+++ main.c
@@ -0,0 +1,25 @@
+//#include <gb/gb.h>
+
+//TODO: rem line on compile
+#include <../../../include/gb/gb.h>
+
+#include <stdint.h>
+
+//init screen
+void Init()
+{
+    //interupts
+    DISPLAY_OFF;
+}
+
+
+//clear screen
+void CLS()
+{
+    DISPLAY_ON;
+}
+
+int main()
+{
+    return 0;
+}
